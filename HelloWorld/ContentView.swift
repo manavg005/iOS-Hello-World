9ddacb70d9454b3d1f890417223cd18dd830aa59
manavg005 <manavguptamg1990@gmail.com>
2021-08-18 02:16:31 +0530

    Hi

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, World!")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .fontWeight(.heavy)
+            .multilineTextAlignment(.leading)
+            
+            
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
